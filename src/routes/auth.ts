856d9db8ed64a1ce8f7d9a2eea5acbe7151c2b31
Clara Battesini <clarabattesini@gmail.com>
2024-01-29 15:56:56 -0500

    feat: refactor google auth functions

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -1,32 +1,21 @@
 import express from "express";
-import googleAuthClient from "../utils/googleAuth";
+import * as googleAuth from "../utils/googleAuth";
 
 const router = express.Router();
 
 router.get("/auth/google", (req, res) => {
-  const url = googleAuthClient.generateAuthUrl({
-    access_type: "offline",
-    scope: [
-      "https://www.googleapis.com/auth/userinfo.profile",
-      "https://www.googleapis.com/auth/userinfo.email",
-    ],
-  });
+  const url = googleAuth.getGoogleAuthUrl();
   res.redirect(url);
 });
 
 router.get("/auth/google/callback", async (req, res) => {
+  const code = req.query.code;
   try {
-    const { tokens } = await googleAuthClient.getToken(
-      req.query.code as string
-    );
-    googleAuthClient.setCredentials(tokens);
+    const user = await googleAuth.getGoogleUser(code as string);
 
-    // Here, you might want to create or update the user in your database
-    // Then create a JWT token and send it back to the client
-
-    res.redirect("/some-frontend-route");
+    // TODO: Save user to database
+    res.send("Authentication successful");
   } catch (error) {
-    console.error("Error getting tokens:", error);
     res.status(500).send("Authentication failed");
   }
 });
--- src/utils/googleAuth.ts
+++ src/utils/googleAuth.ts
@@ -5,8 +5,36 @@ dotenv.config();
 
 const CLIENT_ID = process.env.GOOGLE_OAUTH_CLIENT_ID;
 const CLIENT_SECRET = process.env.GOOGLE_OAUTH_SECRET;
-const REDIRECT_URI = "/auth/google/callback";
+const REDIRECT_URI = "http://localhost:3000/auth/google/callback";
 
 const oauth2Client = new OAuth2Client(CLIENT_ID, CLIENT_SECRET, REDIRECT_URI);
+
+export const getGoogleAuthUrl = () => {
+  return oauth2Client.generateAuthUrl({
+    access_type: "offline",
+    prompt: "consent",
+    scope: [
+      "https://www.googleapis.com/auth/userinfo.email",
+      "https://www.googleapis.com/auth/userinfo.profile",
+    ],
+  });
+};
+
+export const getGoogleUser = async (code: string) => {
+  const { tokens } = await oauth2Client.getToken(code);
+  const googleUserInfoUrl = "https://www.googleapis.com/oauth2/v2/userinfo";
+
+  try {
+    const response: any = await fetch(googleUserInfoUrl, {
+      headers: {
+        Authorization: `Bearer ${tokens.access_token}`,
+      },
+    });
+    return response.data;
+  } catch (error) {
+    console.error("Error fetching user from Google", error);
+    throw new Error("Unable to fetch user information");
+  }
+};
 
 export default oauth2Client;
