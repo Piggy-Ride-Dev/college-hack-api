721bad3e167f6e82f3f9012a72b6e4abbe63544f
Clara Battesini <clarabattesini@gmail.com>
2024-01-29 15:56:29 -0500

    chore: remove console.log

--- src/db.ts
+++ src/db.ts
@@ -3,8 +3,6 @@ import mongoose from "mongoose";
 const MONGO_URI =
   process.env.MONGO_URI || "mongodb://localhost:27017/myDatabase";
 
-console.log("MONGO_URI:", MONGO_URI);
-
 export const connectDB = async () => {
   try {
     await mongoose.connect(MONGO_URI);
