54afac7aab323526bf3e1ba905dc32d4a38de6b8
Clara Battesini <clarabattesini@gmail.com>
2024-02-03 14:34:59 -0500

    feat: route to post documents

---
+++ src/routes/Documents.ts
@@ -0,0 +1,34 @@
+import express from "express";
+import multer from "multer";
+import { multipleUpload } from "../middlewares/Upload";
+
+const router = express.Router();
+
+router.post("/documents/upload", (req, res) => {
+  multipleUpload(req, res, (err) => {
+    if (err instanceof multer.MulterError) {
+      return res
+        .status(500)
+        .send({ error: { message: `Multer uploading error: ${err.message}` } })
+        .end();
+    }
+
+    if (err) {
+      if (err.name == "ExtensionError") {
+        res
+          .status(413)
+          .send({ error: { message: err.message } })
+          .end();
+      } else {
+        res
+          .status(500)
+          .send({
+            error: { message: `unknown uploading error: ${err.message}` },
+          })
+          .end();
+      }
+      return;
+    }
+    return res.status(200).send({ message: "Files uploaded successfully" });
+  });
+});
