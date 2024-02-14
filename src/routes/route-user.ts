2cb81af8c623b0212968f797980822c166a444d8
Clara Battesini <clarabattesini@gmail.com>
2024-02-14 09:44:24 -0500

    feat: handling the req inside the route and not into the gateways

--- src/routes/route-user.ts
+++ src/routes/route-user.ts
@@ -1,10 +1,17 @@
 import express from "express";
-import { authenticateToken } from "../middlewares/mw-auth";
-import * as UserController from "../controllers/ctrl-user";
+import { authenticateToken } from "../middlewares/auth/mw-auth";
+import * as UserGateway from "../gateways/user/gtw-user";
 
 const router = express.Router();
 
-router.get("/:id", authenticateToken, UserController.getUser);
-router.patch("/:id", authenticateToken, UserController.updateUser);
+router.get("/:id", authenticateToken, async (req, res) => {
+  const response = await UserGateway.get(req.params.id);
+  return res.status(response.code).json(response.data);
+});
+
+router.patch("/:id", authenticateToken, async (req, res) => {
+  const response = await UserGateway.patch(req.params.id, req.body.user);
+  return res.status(response.code).json(response.data);
+});
 
 export default router;
