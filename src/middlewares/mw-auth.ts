8e09913acdb602cb2ec80d886948e3866d5750e6
Clara Battesini <clarabattesini@gmail.com>
2024-02-19 14:25:04 -0500

    fix: user data in authorization middleware

--- src/middlewares/mw-auth.ts
+++ src/middlewares/mw-auth.ts
@@ -1,4 +1,5 @@
 import { Request, Response, NextFunction } from "express";
+import { User } from "../models/mdl-user";
 import jwt from "jsonwebtoken";
 
 export const authorizationMiddleware = (
@@ -13,14 +14,18 @@ export const authorizationMiddleware = (
     return res.status(401).send({ message: "User not authenticated" });
   }
 
-  jwt.verify(token, process.env.JWT_SECRET as string, (err, user) => {
+  jwt.verify(token, process.env.JWT_SECRET as string, (err, userData) => {
     if (err && err.name === "TokenExpiredError") {
       return res.status(401).send({ message: "Token expired" });
     }
     if (err) {
       return res.status(403).send({ message: "Invalid token" });
     }
-    req.user = user;
+    if (!userData) {
+      return res.status(401).json({ message: "Authentication failed" });
+    }
+    userData = userData as { user: User };
+    req.user = userData.user;
     next();
   });
 };
