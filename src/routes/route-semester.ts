bb1f6773e51d41e520919fc83e06f67e5fd6622e
Clara Battesini <clarabattesini@gmail.com>
2024-02-19 14:26:57 -0500

    feat: Add routes, controllers and models for semester CRUD

--- src/routes/route-semester.ts
+++ src/routes/route-semester.ts
@@ -1,13 +1,44 @@
-import express from "express";
+import express, { Request, Response } from "express";
 import { authorizationMiddleware } from "../middlewares/mw-auth";
 import * as SemesterController from "../controllers/ctrl-semester";
 
 const router = express.Router();
 
 router.get(
-  "/:userId",
+  "/",
   authorizationMiddleware,
-  SemesterController.getSemestersByUser
+  async (req: Request, res: Response) => {
+    const userId = req.user._id;
+    const response = await SemesterController.getSemesterList(userId);
+    return res.status(response.status).json(response);
+  }
+);
+
+router.get(
+  "/:id",
+  authorizationMiddleware,
+  async (req: Request, res: Response) => {
+    const response = await SemesterController.getSemester(req.params.id);
+    return res.status(response.status).json(response);
+  }
+);
+
+router.post(
+  "/",
+  authorizationMiddleware,
+  async (req: Request, res: Response) => {
+    const response = await SemesterController.createSemester(req.body.semester);
+    return res.status(response.status).json(response);
+  }
+);
+
+router.patch(
+  "/:id",
+  authorizationMiddleware,
+  async (req: Request, res: Response) => {
+    const response = await SemesterController.createSemester(req.body.semester);
+    return res.status(response.status).json(response);
+  }
 );
 
 export default router;
