735521d3e5bebc95623bf2d81ffa314603818877
Clara Battesini <clarabattesini@gmail.com>
2024-02-21 09:29:07 -0500

    feat: moving document upload controller to semester domain

--- src/models/mdl-semester.ts
+++ src/models/mdl-semester.ts
@@ -1,4 +1,4 @@
-import { Schema, model, Document } from "mongoose";
+import { Schema, model, Document, ObjectId } from "mongoose";
 import { WeekDay } from "../utils/util-types";
 
 export type ProcessingFileStatus =
@@ -14,18 +14,22 @@ export interface Schedule {
   location: string;
 }
 
-export interface Section {
-  id?: string;
+export interface Section extends SectionProcessing {
   code: string;
   courseID: string;
   teacherID: string;
-  processingFileStatus: ProcessingFileStatus;
   status: SectionStatus;
   schedules: {
     lecture: Schedule | null;
     lab: Schedule | null;
     office: Schedule | null;
   };
+}
+
+export interface SectionProcessing {
+  _id?: string;
+  processingFileStatus: ProcessingFileStatus;
+  files: string[];
 }
 
 export interface Semester extends Document {
@@ -36,6 +40,20 @@ export interface Semester extends Document {
   start: Date | null;
   end: Date | null;
 }
+
+const sectionSchema = new Schema<Section>({
+  code: { type: String, required: false },
+  courseID: { type: String, required: false },
+  teacherID: { type: String, required: false },
+  status: { type: String, required: false },
+  schedules: {
+    lecture: { type: Object, required: false },
+    lab: { type: Object, required: false },
+    office: { type: Object, required: false },
+  },
+  processingFileStatus: { type: String, required: true },
+  files: { type: [String], required: true },
+});
 
 const semesterSchema = new Schema<Semester>({
   userID: { type: String, required: true },
@@ -49,24 +67,43 @@ const semesterSchema = new Schema<Semester>({
   year: { type: Number, required: true },
 });
 
-export const Semester = model("Semester", semesterSchema);
+export const SemesterModel = model("Semester", semesterSchema);
+export const SectionModel = model("Section", sectionSchema);
 
 export function create(semester: Semester): Promise<Semester> {
-  return Semester.create(semester);
+  return SemesterModel.create(semester);
 }
 
 export function getById(id: string): Promise<Semester | null> {
-  return Semester.findById(id).populate("sections");
+  return SemesterModel.findById(id);
 }
 
 export function getByUserID(userID: string): Promise<Semester[]> {
-  return Semester.find({ userID: userID }).populate("sections");
+  return SemesterModel.find({ userID: userID });
 }
 
-export function update(semester: Semester): Promise<Semester | null> {
-  return Semester.findByIdAndUpdate(semester.id, semester);
+export function update(
+  semesterId: string,
+  semester: Semester
+): Promise<Semester | null> {
+  return SemesterModel.findByIdAndUpdate(semesterId, semester, { new: true });
+}
+
+export async function uploadFile(
+  semesterId: string,
+  fileURL: string
+): Promise<Semester | null> {
+  const newSection = await SectionModel.create({
+    processingFileStatus: "pending",
+    files: [fileURL],
+  });
+  return await SemesterModel.findByIdAndUpdate(
+    semesterId,
+    { $push: { courses: newSection._id } },
+    { new: true }
+  );
 }
 
 export function deleteItem(id: string): Promise<Semester | null> {
-  return Semester.findByIdAndDelete(id);
+  return SemesterModel.findByIdAndDelete(id);
 }
--- src/routes/route-semester.ts
+++ src/routes/route-semester.ts
@@ -1,5 +1,6 @@
 import express, { Request, Response } from "express";
 import { authorizationMiddleware } from "../middlewares/mw-auth";
+import { azureUploadMiddleware, upload } from "../middlewares/mw-upload";
 import * as SemesterController from "../controllers/ctrl-semester";
 
 const router = express.Router();
@@ -37,6 +38,24 @@ router.patch(
   authorizationMiddleware,
   async (req: Request, res: Response) => {
     const response = await SemesterController.createSemester(req.body.semester);
+    return res.status(response.status).json(response);
+  }
+);
+
+router.post(
+  "/:id/upload-files",
+  authorizationMiddleware,
+  upload.array("files", 6),
+  azureUploadMiddleware,
+  async (req: Request, res: Response) => {
+    res.status(200).json({
+      message: "Files uploaded successfully",
+      urls: req.body.uploadedFilesUrls,
+    });
+    const response = await SemesterController.uploadFilesToSemester(
+      req.params.id,
+      req.body.uploadedFilesUrls
+    );
     return res.status(response.status).json(response);
   }
 );
