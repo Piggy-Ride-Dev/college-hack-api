3d1eecb304317fd01a689f89eb5d29fad2303bd8
Clara Battesini <clarabattesini@gmail.com>
2024-02-21 10:26:29 -0500

    fix: user route using userId from req.user and not params

--- src/routes/route-user.ts
+++ src/routes/route-user.ts
@@ -6,23 +6,22 @@ import * as UserController from "../controllers/ctrl-user";
 const router = express.Router();
 
 router.get(
-  "/:id",
+  "/",
   authorizationMiddleware,
   async (req: Request, res: Response) => {
-    const response = await UserController.getUser(req.params.id);
+    const userId = req.user._id;
+    const response = await UserController.getUser(userId);
     if (response.isError())
       return res.status(response.status).send(response.message);
     else res.send(response.data);
   }
 );
 router.patch(
-  "/:id",
+  "/",
   authorizationMiddleware,
   async (req: Request, res: Response) => {
-    const response = await UserController.updateUser(
-      req.params.id,
-      req.body.user
-    );
+    const userId = req.user._id;
+    const response = await UserController.updateUser(userId, req.body.user);
     if (response.isError())
       return res.status(response.status).send(response.message);
     else res.send(response.data);
